825fd27a8690857b5b6ea14baba3dae6c0ae0339
Vladyslav Pnyevskyy <226667@student.uek.krakow.pl>
2022-10-10 18:17:20 +0200

    variables1

---
+++ 02-TypesAndVariables/exercise7-8.py
@@ -0,0 +1,17 @@
+n1 = 5
+n2 = 1
+n3 = 8
+n4 = 6
+n5 = 3
+x  = n1+n2+n3+n4+n5
+y = n1**2 + n2**2 + n3**2 + n4**2 + n5**2
+z = n3/n5
+q = n3==n4
+print(x, y, z, q)
+
+u=34
+o=7
+p=o
+o=u
+u=p
+print(u, o)
