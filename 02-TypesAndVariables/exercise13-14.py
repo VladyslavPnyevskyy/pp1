e131582a1a8d3b17d0621266a6b3514b148c490c
Vladyslav Pnyevskyy <226667@student.uek.krakow.pl>
2022-10-10 21:51:43 +0200

    Programs

---
+++ 02-TypesAndVariables/exercise13-14.py
@@ -0,0 +1,16 @@
+import math
+
+
+Radius = 5
+math.pi
+#Pi = 3.14
+Area = math.pi*Radius**2
+Circumference = 2*math.pi*Radius
+print(Area, Circumference) 
+
+
+CelciusInput = input('enter your temperature in celcius: ')
+Celcius = int(CelciusInput)
+Kelvin = Celcius + 273.15
+Farengeit= 1.8*Celcius+35
+print (Kelvin, Farengeit)
