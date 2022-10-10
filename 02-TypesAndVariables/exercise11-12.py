3225730caef14581206fb7e3cee91b5a474f01d2
Vladyslav Pnyevskyy <226667@student.uek.krakow.pl>
2022-10-10 21:17:49 +0200

    DataOutput

---
+++ 02-TypesAndVariables/exercise11-12.py
@@ -0,0 +1,9 @@
+Hight = 184
+Name = "Wladyslaw"
+Age = 19
+print(f"My name is {Name}, I'm {Age} years old, and my height is {Hight} cm")
+
+Number = 4
+NumberTo = Number**2
+
+print('The value is {}, and {} is its second power'.format( Number, NumberTo))
