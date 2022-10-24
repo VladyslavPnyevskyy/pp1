0415440b8cb2a6949680b6c040298f8bc3e421cf
Vladyslav Pnyevskyy <226667@student.uek.krakow.pl>
2022-10-24 09:50:57 +0200

    before class

---
+++ 04-Subroutines/BeforeClass/computergrades.py
@@ -0,0 +1,25 @@
+def computegrade(Number):
+    try:
+    
+        if Number>1:
+            print("Niepoprawna wartość")
+        elif Number>=0.9:
+            print("5,0")
+        elif Number>=0.8:
+            print("4,5")
+        elif Number>=0.7:
+            print("4,0")
+        elif Number>=0.6:
+            print("3,5")
+        elif Number >=0.5:
+            print("3,0")
+        elif Number<0:
+            print("Niepoprawna wartość")
+        else: 
+            print("2.0")
+
+    except: 
+        print("wprowadź liczbe")
+
+
+computegrade(0.3)
